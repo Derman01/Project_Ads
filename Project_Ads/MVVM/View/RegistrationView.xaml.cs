b62f12e39fccbfa7e988e2548dd49ad1eca82605
Gay <konako.osu@gmail.com>
2021-12-07 04:54:04 +0500

    Закончил интерфейс
    - пофиксил все кнопки в тайтлбаре
    - дополнил авторизацию
    - добавил регистрацию
    - добавил мои объявления
    - застайлил приколдесный TextBox

---
+++ Project_Ads/MVVM/View/RegistrationView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Project_Ads.MVVM.ViewModel;
+
+namespace Project_Ads.MVVM.View
+{
+    /// <summary>
+    /// Логика взаимодействия для RegistrationView.xaml
+    /// </summary>
+    public partial class RegistrationView : UserControl
+    {
+        public RegistrationView()
+        {
+            InitializeComponent();
+        }
+
+        private void OnRegisterUser(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void ProfileViewOpen(object sender, RoutedEventArgs e)
+        {
+            MainViewModel.ProfileViewСommand.Execute(sender);
+        }
+    }
+}
